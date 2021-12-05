7a41525196e80cca3916df07b4a3f8305c481b35
somer1221 <79031075+somer1221@users.noreply.github.com>
2021-12-05 10:35:32 -0500

    Add files via upload

---
+++ living.js
@@ -0,0 +1,61 @@
+image1 = "";
+statusofmodel = "";
+newresults = [];
+alertsound = ""
+
+function preload() {
+   image1 = loadImage('living room.jpg');
+   alertsound = loadSound('living room.jpg')
+}
+
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Model Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Cocossd has been initialized");
+    statusofmodel = true;
+    
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+        console.log("Error occured.");
+    }
+        console.log(results);
+        console.log("Results sent!");
+        newresults = results;
+    }
+
+function draw() {
+  image(image1, 0, 0, 640, 420);
+    
+   if(statusofmodel != "") {
+    objectDetector.detect(image1, gotResults);
+    for(i=0;i<results.length;i++) {
+        console.log("XADJSLA;JDLAKSJDLAKSJDLAJSKDJALJSLDJHAHWKDSGDSAGDHKSGJHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+     document.getElementById("status").innerHTML = "Model Status: Objects Detected";
+     fill("#FF0000");
+        percentage = floor(results[i].confidence * 100);
+        text(results[i].label + " " + percentage + "%", results[i].x, results[i].y);
+        noFill();
+        stroke("#FF0000");
+        rect(results[i].x, results[i].y, results[i].width, results[i].height);
+    }
+  }
+
+  if(results[0].label = "person") {
+      document.getElementById("status").innerHTML = "Model Status: Baby Detected";
+      alertsound.stop()
+  }
+  else {
+    document.getElementById("status").innerHTML = "Model Status: Baby Detected";
+  }
+}
+ 
+
+
